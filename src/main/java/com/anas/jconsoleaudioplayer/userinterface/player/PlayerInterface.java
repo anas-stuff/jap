98d03205651bf75cb204fc7fe3c567b981d755cc
Rosyparadise <cevangel@csd.auth.gr>
2022-02-08 22:58:33 +0200

    -fixed index of currently playing file so it follows the enumeration of the playlist -changed menu print "(q)uit" to "exit" just like the manual -fixed some grammatical errors and updated the manual :D

--- src/main/java/com/anas/jconsoleaudioplayer/userinterface/player/PlayerInterface.java
+++ src/main/java/com/anas/jconsoleaudioplayer/userinterface/player/PlayerInterface.java
@@ -127,7 +127,7 @@ public class PlayerInterface extends Screen {
     private Action getTheStaticAction(String input) {
         return switch (input) {
             case "p" -> Action.PLAY;
-            case "pu" -> Action.PAUSE;
+            case "pa" -> Action.PAUSE;
             case "re" -> Action.RESUME;
             case "s" -> Action.STOP;
             case "n" -> Action.NEXT;
@@ -141,15 +141,15 @@ public class PlayerInterface extends Screen {
             case "v+" -> Action.VOLUME_UP;
             case "v-" -> Action.VOLUME_DOWN;
             case "open" -> Action.OPEN_FILE_BROWSER;
-            case "q" -> Action.EXIT;
+            case "exit" -> Action.EXIT;
             default -> Action.UNKNOWN;
         };
     }
 
     private void printTheOptions() {
-        System.out.println("(p)lay, (pu)se, (re)sume, (s)top, (n)ext, (pr)ivos, (l)oop, (lp)loop play list, (sh)uffle\n" +
+        System.out.println("(p)lay, (pa)use, (re)sume, (s)top, (n)ext, (pr)evious, (l)oop, (lp)loop play list, (sh)uffle\n" +
                 "(m)ute, (vl) show volume level,(v:) set volume, (v+) volume up(+10), (v-)volume down(-10)" +
-                ", (open) Open file browser, (:) Search, (q)uit");
+                ", (open) Open file browser, (:) Search, (exit) Exit from program");
         System.out.print("> ");
     }
 
