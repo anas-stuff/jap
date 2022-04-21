749ed88b13b81fce48167a684672715ee85005a3
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-04-21 23:56:30 +0200

    Add the java doc

--- src/main/java/com/anas/jconsoleaudioplayer/player/PlayerListener.java
+++ src/main/java/com/anas/jconsoleaudioplayer/player/PlayerListener.java
@@ -1,5 +1,9 @@
 package com.anas.jconsoleaudioplayer.player;
 
 public interface PlayerListener {
+    /**
+     * Called when the player has any change, such as the player state(playing, paused, stopped)
+     * @param event the event
+     */
     void onPlayerEvent(PlayerEvent event);
 }
