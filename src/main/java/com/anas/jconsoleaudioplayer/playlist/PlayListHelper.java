098a4714164527c3e824115c58de61546a1b0f0e
Anas Elgarhy <anas.elgarhy.dev@gmail.com>
2022-05-04 21:06:56 +0200

    Fix sync bug

--- src/main/java/com/anas/jconsoleaudioplayer/playlist/PlayListHelper.java
+++ src/main/java/com/anas/jconsoleaudioplayer/playlist/PlayListHelper.java
@@ -1,12 +1,30 @@
 package com.anas.jconsoleaudioplayer.playlist;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 
-public class PlayListHelper {
-    public static void addAllToPlayList(PlayList targetPlayList, File[] files) {
-        for (File file : files) {
-           Track track = new Track(0, file);
-            targetPlayList.add(track);
+public class PlayListLoader {
+    private final ObjectMapper mapper;
+    private static PlayListLoader instance;
+
+    private PlayListLoader() {
+        mapper = new ObjectMapper();
+    }
+
+    public static PlayListLoader getInstance() {
+        if (instance == null) {
+            instance = new PlayListLoader();
         }
+        return instance;
+    }
+
+    public PlayList load(File playlistFile) {
+        try {
+            return mapper.readValue(playlistFile, PlayList.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
